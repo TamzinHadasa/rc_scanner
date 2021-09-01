964932d31f06db83454397f2818059501a386643
Tamzin Hadasa Kelly <coding@tamz.in>
2021-09-01 00:40:23 -0400

    add documentation to `flaglog`

--- flaglog.py
+++ flaglog.py
@@ -20,23 +20,42 @@ def _manager(mode: OpenTextMode) -> Generator[TextIO, None, None]:  # pylint: di
 
 
 def read() -> list[FlagLogEntry]:
+    """Read the flagged-changes log as JSON.
+
+    Returns:
+      A list of FlagLogEntry objects.
+    """
     with _manager('r') as logfile:
         return json.load(logfile)
 
 
 def write(data: list[FlagLogEntry]) -> None:
+    """Write JSON to the flagged-changes log.
+
+    CAUTION: Overwrites existing contents.
+
+    Arg:
+      data:  A list of FlagLogEntry objects to overwrite the flagged-
+        changes log with.
+    """
     assert data, "Tried to save data but data was empty."
     with _manager('w') as logfile:
         json.dump(data, logfile, indent=4)
 
 
 def init() -> None:
+    """Initialize the flagged-changes log as an empty list."""
     with _manager('r+') as logfile:
         if not logfile.read():
             logfile.write('[]')
 
 
 def append(entry: FlagLogEntry) -> None:
+    """Add a FlagLogEntry to the flagged-changes log.
+
+    Arg:
+      entry:  A FlagLogEntry to append.
+    """
     data = read()
     data.append(entry)
     write(data)
